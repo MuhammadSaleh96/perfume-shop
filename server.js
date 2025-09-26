c851bd617e814c89324a674d85e90234bbd8ed4c
Muhammad Saleh <muhammadsaleh1996@yahoo.com>
2025-09-26 04:55:07 +0300

    Initial commit of Perfume Shop

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Use the OpenShift port if set, otherwise default to 8080
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Perfume Shop</h1>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
